63bed5cce6e66ddefc733da25190d3d835aa3873
dzubchik <dzubchik@users.noreply.github.com>
2020-04-22 22:29:52 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ set -ex
 
 git config user.name "Travis Deploy"
 git config user.email "deploy@travis-ci.org"
-git remote add gh-token "https://${GH_TOKEN}@github.com/paycoreio/openfintech-docs.git"
+git remote add gh-token "https://${GH_TOKEN}@github.com/openfintech-io/openfintech-docs.git"
 git fetch gh-token && git fetch gh-token gh-pages:gh-pages
 mkdocs gh-deploy -v --clean --remote-name gh-token
